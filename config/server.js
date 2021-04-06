77f3e4e7ba0128cdb39e32b5d8ecf01b890ac0e4
hannah-devine <hannah96@hotmail.be>
2021-04-06 10:21:00 +0200

    erop

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'f6b9f0e9bb10595adc400c3ca1502446'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '06d76e92-9e06-41c0-8dfc-9676c9dea19a'
+};
